7b280c163f9e87f747f6e3fd4d2249573821150a
Matthew Kauth <mkauth@carthage.edu>
2013-09-04 15:27:46 -0500

    the lads are back in control... sort off

--- djreggie/changemajor/urls.py
+++ djreggie/changemajor/urls.py
@@ -6,3 +6,4 @@ urlpatterns = patterns('djreggie.changemajor.views', #Look in my 'views.py' file
     url(r'^$', 'index', name="index"), #If I have nothing else appended to my url, I go into my 'views.py' file and call the 'index' function
     
 )
+#test comment
