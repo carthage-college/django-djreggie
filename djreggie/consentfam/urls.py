fcb3a2d0148e2742a63ec611994d0d08ecb50c98
Matthew Kauth <mkauth@carthage.edu>
2013-09-13 16:09:20 -0500

    the lads are back in control

--- djreggie/consentfam/urls.py
+++ djreggie/consentfam/urls.py
@@ -4,3 +4,5 @@ from django.conf.urls import patterns, include, url #Need this
 urlpatterns = patterns('djreggie.consentfam.views', #Look in my 'views.py' file too
     url(r'^$', 'create', name="create"), #If I have nothing else appended to my url, I go into my 'views.py' file and call the 'index' function
 )    
+
+#THIS IS SOMETHIN TO COMMIT
