599a613b2a2748bb83a1f172a836b51873f82049
Joseph Sawyer (zorpix) <uberelitepwnzor@gmail.com>
2014-06-16 11:23:50 -0500

    minor sprucing

--- djreggie/consentfam/models.py
+++ djreggie/consentfam/models.py
@@ -1,6 +1,4 @@
-#You want these.
 from django.db import models
-from django import forms
 
 #Each class is a model that will be turned into a form
 class Contact(models.Model):
@@ -21,7 +19,9 @@ class ConsentModel(models.Model):
     )
 
     #Please_accept = models.CharField(choices=CHOICES1, max_length=200)
-    Which_information_would_you_like_to_share = models.CharField(choices=CHOICES2, max_length=2000)
+    Which_information_would_you_like_to_share = models.CharField(choices=CHOICES2,
+                                                                 max_length=2000,
+                                                                 verbose_name='Which information would you like to share?')
     Full_Name_of_Student = models.CharField(max_length=100, verbose_name='Name')
     Carthage_ID_Number = models.IntegerField(max_length=7, verbose_name='Student ID')
     Date = models.DateField(auto_now_add=True)
