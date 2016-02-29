05b2d812cf3108c2cf5f9155e02c1a27585f8fee
Bender <plungerman@gmail.com>
2016-02-29 08:47:29 -0600

    removed unnecessary comments

--- djreggie/changemajor/urls.py
+++ djreggie/changemajor/urls.py
@@ -1,12 +1,9 @@
-#Need this
 from django.conf.urls import patterns, include, url
 
-#Where I put all the 'views' associated with this form
-urlpatterns = patterns('djreggie.changemajor.views',#Look in my 'views.py' file too
+urlpatterns = patterns('djreggie.changemajor.views',
     url(r'^admin/?$', 'admin', name="admincm"),
     url(r'^admin/student/(?P<changemajor_no>[0-9]+)/?$', 'student', name="studentcm"),
     url(r'^admin/search/?$', 'search', name="searchcm"),
-    url(r'^set_approved/$', 'set_approved', name='cm_set_approved'),    
-    url(r'^$', 'create'), #If I have nothing else appended to my url, I go into my 'views.py' file and call the 'index' function
-    
+    url(r'^set_approved/$', 'set_approved', name='cm_set_approved'),
+    url(r'^$', 'create'),
 )
