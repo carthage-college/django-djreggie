0e0c4cf9e6aafc9b47e7ef78445a02bf65599e03
Bender <plungerman@gmail.com>
2016-02-29 08:43:59 -0600

    switched to django1.8

--- djreggie/wsgi_default.py
+++ djreggie/wsgi_default.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for djreggie project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os, sys
 
 sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/data2/django_1.5.1/')
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+sys.path.append('/usr/local/lib/python2.7/dist-packages/')
+sys.path.append('/data2/django_1.8')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
 
@@ -19,15 +12,15 @@ os.environ.setdefault("PYTHON_EGG_CACHE", "/var/cache/python/.python-eggs")
 os.environ.setdefault("TZ", "America/Chicago")
 
 # informix
-os.environ['INFORMIXSERVER'] = 'wilson'
-os.environ['DBSERVERNAME'] = 'wilson'
-os.environ['INFORMIXDIR'] = '/opt/ibm/informix'
-os.environ['ODBCINI'] = '/etc/odbc.ini'
-os.environ['ONCONFIG'] = 'onconf.carstrain'
-os.environ['INFORMIXSQLHOSTS'] = '/opt/ibm/informix/etc/sqlhosts'
+os.environ['INFORMIXSERVER'] = ''
+os.environ['DBSERVERNAME'] = ''
+os.environ['INFORMIXDIR'] = ''
+os.environ['ODBCINI'] = ''
+os.environ['ONCONFIG'] = ''
+os.environ['INFORMIXSQLHOSTS'] = ''
 
-os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
-os.environ['LD_RUN_PATH'] = '/opt/ibm/informix/lib:/opt/ibm/informix/lib/esql:/opt/ibm/informix/lib/tools:/usr/lib/apache2/modules'
+os.environ['LD_LIBRARY_PATH'] = ''
+os.environ['LD_RUN_PATH'] = ''
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
