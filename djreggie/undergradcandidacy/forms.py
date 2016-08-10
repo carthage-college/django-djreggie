9fb65927d7205dfe504caa11832e0bb4ff93e73f
plungerman <plungerman@gmail.com>
2016-08-10 11:34:54 -0500

    best_contact variable is now best_contact_value

--- djreggie/undergradcandidacy/forms.py
+++ djreggie/undergradcandidacy/forms.py
@@ -74,17 +74,20 @@ class UndergradForm(forms.ModelForm):
             if not cleaned_data.get('term'):
                 self.errors['term'] = self.error_class(['This field is required'])
 
-        if cleaned_data.get('best_contact') == 'EML':
-            if not re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', cleaned_data.get('best_contact_value')):
-                self.errors['best_contact_value'] = self.error_class(['You must enter a valid email address'])
-                del cleaned_data['best_contact_value']
-        elif cleaned_data.get('best_contact') == 'PHN':
-            if not re.match(r'^((?:1?[\s\-\.\/]?\(?(?:\d{3})\)?)?[\s\-\.\/]?\d{3}[\s\-\.\/]?\d{4}(?:\s?(?:x|ext|\.)?\s?\d{4})?)$',
-                            cleaned_data.get('best_contact_value')):
-                self._errors['best_contact_value'] = self.error_class(['You must enter a valid phone number'])
-                del cleaned_data['best_contact_value']
+        if cleaned_data.get('best_contact_value') != None:
+            if cleaned_data.get('best_contact') == 'EML':
+                if not re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', cleaned_data.get('best_contact_value')):
+                    self.errors['best_contact_value'] = self.error_class(['You must enter a valid email address'])
+                    del cleaned_data['best_contact_value']
+            elif cleaned_data.get('best_contact') == 'PHN':
+                if not re.match(r'^((?:1?[\s\-\.\/]?\(?(?:\d{3})\)?)?[\s\-\.\/]?\d{3}[\s\-\.\/]?\d{4}(?:\s?(?:x|ext|\.)?\s?\d{4})?)$',
+                                cleaned_data.get('best_contact_value')):
+                    self._errors['best_contact_value'] = self.error_class(['You must enter a valid phone number'])
+                    del cleaned_data['best_contact_value']
+            else:
+                self.errors['best_contact'] = self.error_class(['This field is required'])
         else:
-            self.errors['best_contact'] = self.error_class(['This field is required'])
+            self.errors['best_contact_value'] = self.error_class(['Contact value is required'])
 
         return cleaned_data
 
