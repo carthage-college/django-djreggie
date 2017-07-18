77365b55db03bc04aad8539a7b01085ce2d5ee4a
plungerman <plungerman@gmail.com>
2017-07-18 14:03:34 -0500

    begin code clean up in preparation for new form for advisors that will allow them to fill out the change of major form for students

--- djreggie/changemajor/models.py
+++ djreggie/changemajor/models.py
@@ -1,19 +1,29 @@
-#Need this
+from django.conf import settings
 from django.db import models
-from djzbar import settings
-from djreggie import settings
-from sqlalchemy import create_engine
+
 from djzbar.utils.informix import do_sql
 
 class ChangeModel(models.Model):
-    student_id = models.CharField(max_length=7, blank=False, db_column='student_id')
-    name = models.CharField(max_length=200, blank=True) #'blank=False' means the field is required
-    majorlist = models.CharField(max_length=1000)
-    minorlist = models.CharField(max_length=1000, blank=True, null=True)
-    advisor = models.CharField(max_length=200, null=True, blank=True)
+    student_id = models.CharField(
+        max_length=7, blank=False, db_column='student_id'
+    )
+    name = models.CharField(
+        max_length=200, blank=True
+    )
+    majorlist = models.CharField(
+        max_length=1000
+    )
+    minorlist = models.CharField(
+        max_length=1000, blank=True, null=True
+    )
+    advisor = models.CharField(
+        max_length=200, null=True, blank=True
+    )
 
-    #Majors
-    #2015-10-30: Brigid requested that nursing not be included in the list of available majors
+    # Majors
+    # 2015-10-30: Brigid requested that nursing not be included
+    # in the list of available majors
+
     getMajorSQL = '''
         SELECT
             TRIM(txt) AS txt, TRIM(major) AS major
@@ -30,10 +40,16 @@ class ChangeModel(models.Model):
         ORDER BY
             txt ASC
     '''
-    major = do_sql(getMajorSQL, key=settings.INFORMIX_DEBUG, earl=settings.INFORMIX_EARL)
+
+    major = do_sql(
+        getMajorSQL,
+        key=settings.INFORMIX_DEBUG,
+        earl=settings.INFORMIX_EARL
+    )
+
     MAJOR_CHOICES = tuple((row['major'], row['txt']) for row in major)
 
-    #Minors
+    # Minors
     getMinorSQL = '''
         SELECT
             TRIM(txt) AS txt, TRIM(minor) AS minor
@@ -48,21 +64,67 @@ class ChangeModel(models.Model):
         ORDER BY
             txt ASC
     '''
-    minor = do_sql(getMinorSQL, key=settings.INFORMIX_DEBUG, earl=settings.INFORMIX_EARL)
+
+    minor = do_sql(
+        getMinorSQL,
+        key=settings.INFORMIX_DEBUG,
+        earl=settings.INFORMIX_EARL
+    )
+
     MINOR_CHOICES = tuple((row['minor'], row['txt']) for row in minor)
 
-    major1 = models.CharField(max_length=200, choices=MAJOR_CHOICES, db_column='major1')
-    minor1 = models.CharField(max_length=200, choices=MINOR_CHOICES, null=True, blank=True, db_column='minor1')
-    major2 = models.CharField(max_length=200, choices=MAJOR_CHOICES, null=True, blank=True, db_column='major2')
-    minor2 = models.CharField(max_length=200, choices=MINOR_CHOICES, null=True, blank=True, db_column='minor2')
-    major3 = models.CharField(max_length=200, choices=MAJOR_CHOICES, null=True, blank=True, db_column='major3')
-    minor3 = models.CharField(max_length=200, choices=MINOR_CHOICES, null=True, blank=True, db_column='minor3')
-   
+    major1 = models.CharField(
+        max_length=200,
+        choices=MAJOR_CHOICES, db_column='major1'
+    )
+    minor1 = models.CharField(
+        max_length=200,
+        choices=MINOR_CHOICES, db_column='minor1',
+        null=True, blank=True
+    )
+    major2 = models.CharField(
+        max_length=200,
+        choices=MAJOR_CHOICES, db_column='major2',
+        null=True, blank=True
+    )
+    minor2 = models.CharField(
+        max_length=200,
+        choices=MINOR_CHOICES, db_column='minor2',
+        null=True, blank=True
+    )
+    major3 = models.CharField(
+        max_length=200,
+        choices=MAJOR_CHOICES, db_column='major3',
+        null=True, blank=True
+    )
+    minor3 = models.CharField(
+        max_length=200,
+        choices=MINOR_CHOICES, db_column='minor3',
+        null=True, blank=True
+    )
+
     def __unicode__(self):
         return self.name
 
     def save(self):
-        #put data in staging tables
-        insertSQL = '''INSERT INTO cc_stg_changemajor (student_id, major1, major2, major3, minor1, minor2, minor3, advisor_id, datecreated)
-        VALUES (%(student_id)s, "%(major1)s", "%(major2)s", "%(major3)s", "%(minor1)s", "%(minor2)s", "%(minor3)s", "%(advisor)s", CURRENT)''' % (self.__dict__)
-        do_sql(insertSQL, key=settings.INFORMIX_DEBUG, earl=settings.INFORMIX_EARL)
+
+        # put data in staging tables
+
+        insertSQL = '''
+            INSERT INTO
+                cc_stg_changemajor (
+                    student_id, major1, major2, major3, minor1,
+                    minor2, minor3, advisor_id, datecreated
+                )
+            VALUES (
+                "{'student_id'}", "{'major1'}", "{'major2'}",
+                "{'major3'}", "{'minor1'}", "{'minor2'}", "{'minor3'}",
+                "{'advisor'}", CURRENT
+            )
+        '''.format(self.__dict__)
+
+        do_sql(
+            insertSQL,
+            key=settings.INFORMIX_DEBUG,
+            earl=settings.INFORMIX_EARL
+        )
